e1d5877c616de23c9bd2813762ca3f4a384fcd1e
Bustos S <97401841+bustos00@users.noreply.github.com>
2022-02-17 12:13:49 -0300

    Update db_cidade_das_frutas.sql

--- db_cidade_das_frutas.sql
+++ db_cidade_das_frutas.sql
@@ -21,35 +21,35 @@ create table tb_categoria (
 	estado varchar (255), 
    	peso varchar (255),	
 	valor double,
-	fk_produtos bigint,
+	fk_categoria bigint,
 	primary key (id),
-	foreign key (fk_produtos) references tb_categoria (id)
+	foreign key (fk_categoria) references tb_categoria (id)
 	); 
 	
-	insert into tb_produtos (nome, estado, peso, valor, fk_produtos) values ("Feijao", "Bom", "um killo", 22.3 , 1) ; 
+	insert into tb_produtos (nome, estado, peso, valor, fk_categoria) values ("Feijao", "Bom", "um killo", 22.3 , 1) ; 
 
-	insert into tb_produto (nome, estado, peso, valor, fk_produto) values ("tomate", "Madura" , "2 killo" , 62.3 , 2);
+	insert into tb_produtos (nome, estado, peso, valor,  fk_categoria) values ("tomate", "Madura" , "2 killo" , 62.3 , 2);
 
-	insert into tb_produto (nome, estado, peso, valor, fk_produto) values ("cereja", "Imadura" , " oitocentos grama" , 12.23 , 2);
+	insert into tb_produtos (nome, estado, peso, valor,  fk_categoria) values ("cereja", "Imadura" , " oitocentos grama" , 12.23 , 2);
 
-	insert into tb_produto (nome, estado, peso, valor, fk_produto) values ("ameixa", "Verde" , "trezentos grama" , 24.23 , 3);
+	insert into tb_produtos (nome, estado, peso, valor,  fk_categoria) values ("ameixa", "Verde" , "trezentos grama" , 24.23 , 3);
 
-	insert into tb_produto (nome, estado, peso, valor, fk_produto) values ("castanha", "Delicia" , "oitenta grama" , 42.23 , 1);
+	insert into tb_produtos (nome, estado, peso, valor,  fk_categoria) values ("castanha", "Delicia" , "oitenta grama" , 42.23 , 1);
 
-	insert into tb_produto (nome, estado, peso, valor, fk_produto) values ("Abacaxi", "Mel" , " um killo" , 11.23 , 4);
+	insert into tb_produtos (nome, estado, peso, valor,  fk_categoria) values ("Abacaxi", "Mel" , " um killo" , 11.23 , 4);
 
-	insert into tb_produto (nome, estado, peso, valor, fk_produto) values("Amora", "Madura" , " um killo"  , 22.23 , 4);
+	insert into tb_produtos (nome, estado, peso, valor,  fk_categoria) values("Amora", "Madura" , " um killo"  , 22.23 , 4);
 
-	insert into tb_produto (nome, estado, peso, valor, fk_produto) values("azeitona", " um killo" , 1 , 44.23 , 3);
+	insert into tb_produtos (nome, estado, peso, valor,  fk_categoria) values("azeitona", " um killo" , 1 , 44.23 , 3);
 
 	
-	 select * from tb_produto where valor > 50 ;
+	 select * from tb_produtos where valor > 50 ;
 
-	 select * from tb_produto where valor between 3 and 60 ; 
+	 select * from tb_produtos where valor between 3 and 60 ; 
 
-	select * from tb_produto where nome like "%C%";
+	select * from tb_produtos where nome like "%C%";
 
-	     select * from tb_produto inner join tb_categoria on tb_categoria.id = tb_produto.fk_produto ;
+	     select * from tb_produtos inner join tb_categoria on tb_categoria.id = tb_produtos.fk_categoria ;
 
- select * from tb_categoria where familia = "fruta" ;
+ select * from tb_produtos where  fk_categoria = 3 ;
 	 
